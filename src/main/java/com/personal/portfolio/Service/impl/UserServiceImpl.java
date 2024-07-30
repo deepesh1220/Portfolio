8ff2258126c533fe23075da077b09c941f392bb2
Deepesh Sahu <sahudeepesh46@gmail.com>
2024-07-30 10:55:03 +0530

    user model

--- src/main/java/com/personal/portfolio/Service/impl/UserServiceImpl.java
+++ src/main/java/com/personal/portfolio/Service/impl/UserServiceImpl.java
@@ -1,6 +1,17 @@
 package com.personal.portfolio.Service.impl;
 
+import com.personal.portfolio.Model.Users;
+import com.personal.portfolio.Repository.UserRepository;
 import com.personal.portfolio.Service.inter.UserService;
+import org.hibernate.mapping.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+    public List getUsers() {
+        return userRepository.findAll()   }
 }
