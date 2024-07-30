e2e69de23e7808cd8623b2af027b1b33bba891f7
Deepesh Sahu <sahudeepesh46@gmail.com>
2024-07-30 13:36:06 +0530

    All DTO is Added

---
+++ src/main/java/com/personal/portfolio/Dto/ContactDTO.java
@@ -0,0 +1,17 @@
+package com.personal.portfolio.Dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ContactDTO {
+    private Long contactId;
+    private String contactType;
+    private String contactValue;
+}
---
+++ src/main/java/com/personal/portfolio/Dto/SkillDTO.java
@@ -0,0 +1,17 @@
+package com.personal.portfolio.Dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class SkillDTO {
+    private Long skillId;
+    private String name;
+    private String category;
+}
